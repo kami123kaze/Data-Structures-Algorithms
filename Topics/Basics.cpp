dab75b2024c2df3d387f000794feaca87dad3145
daksh chauhan <dakshchauhan2002@gmail.com>
2024-04-06 20:23:11 +0530

    Basic Data Structures

---
+++ Topics/Basics.cpp
@@ -0,0 +1,43 @@
+//! The iostream library is an object-oriented library that provides input and output functionality using streams.
+#include <iostream>
+
+//? In int main, the int signifies that the function returns an integer to the operating system upon completion,In int main, the int signifies that the function returns an integer to the operating system upon completion.
+int main(){
+//* DATA TYPES
+
+int; //? defines an integer data type with the size of 2 of 4 bytes.
+
+float; //? defines a floating point data type with a size of 4 bytes.
+
+double; //? defines a double floating point data type with a size of 8 bytes.
+
+char ; //? defines a character data type with a size of 1 byte.
+
+wchar_t; //? defines a wide character data type with size of 2 bytes.
+
+bool; //? defines a boolean data type with a size of 1 byte.
+
+//! MODIIFIED DATA TYPES
+
+signed int; //? 	used for integers (equivalent to int).Size 4 bytes.
+
+unsigned int;	//? can only store positive integers.Size 4 bytes.
+
+short; //?	used for small integers (range -32768 to 32767).Size 2 bytes.
+
+unsigned short;	//?	used for small positive integers (range 0 to 65,535)
+
+long; //?	used for large integers (equivalent to long int).Size is at least 4
+
+unsigned long; //? used for large positive integers or 0 (equivalent to unsigned long int).Size is 4 bytes.
+
+long long; //? used for very large integers (equivalent to long long int).Size 8 bytes.
+
+unsigned long long;	//? used for very large positive integers or 0 (equivalent to unsigned long long int).Size 8 bytes.
+
+long double; //?	used for large floating-point numbers.Size 12 bytes
+
+signed char;	//?	used for characters (guaranteed range -127 to 127).Size 1 byte.
+
+unsigned char;	//?	used for characters (range 0 to 255).Size 1 byte.
+}
