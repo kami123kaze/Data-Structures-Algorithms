1fe7ae7afa237d7f0fc466e9d247fad4373b7012
daksh chauhan <dakshchauhan2002@gmail.com>
2024-06-11 20:41:45 +0530

    Binary Search

---
+++ Topics/Binary-Search.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int BinarySearch(int arr[]){
+
+}
+
+
+
+
+int main(){
+  //? Binary Search Algorithm is a searching algorithm used in a sorted array by repeatedly dividing the search interval in half. The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(log N). 
+
+  //! suppose we are given a sorted array 'arr' and we have to find an element inside with O(Log N) time complexity.
+
+  int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+   BinarySearch(arr);
+}
